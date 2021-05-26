4ea7f9e9e2c97bb1ecd957911cd01f485285f2c6
boba <83132012+boobafinance@users.noreply.github.com>
2021-05-26 16:06:11 -0300

    init

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,38 @@
+export default {
+  cake: {
+    56: '0x0',
+    97: '0xFecBE190631196e73A879b317D8A51CC4fE6cB0B',
+  },
+  life: {
+    56: '0x0',
+    97: '0xFecBE190631196e73A879b317D8A51CC4fE6cB0B',
+  },
+  masterChef: {
+    56: '0x0',
+    97: '0x5eAdf1a622B441Fb53D2b04D2DBE08078c9b0Bc6',
+  },
+  lifeMasterChef: {
+    56: '0x0',
+    97: '0x5eAdf1a622B441Fb53D2b04D2DBE08078c9b0Bc6',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee',
+  },
+}
---
+++ src/utils/addressHelpers.ts
@@ -0,0 +1,28 @@
+import addresses from 'config/constants/contracts'
+
+const chainId = process.env.REACT_APP_CHAIN_ID
+
+export const getCakeAddress = () => {
+  return addresses.cake[chainId]
+}
+export const getLifeAddress = () => {
+  return addresses.life[chainId]
+}
+export const getMasterChefAddress = () => {
+  return addresses.masterChef[chainId]
+}
+export const getLifeMasterChefAddress = () => {
+  return addresses.lifeMasterChef[chainId]
+}
+export const getMulticallAddress = () => {
+  return addresses.mulltiCall[chainId]
+}
+export const getWbnbAddress = () => {
+  return addresses.wbnb[chainId]
+}
+export const getLotteryAddress = () => {
+  return addresses.lottery[chainId]
+}
+export const getLotteryTicketAddress = () => {
+  return addresses.lotteryNFT[chainId]
+}
